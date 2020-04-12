2ba2ba70d6cb81e0794a45662cfa013c82a7539b
Marcelina <m.oscislowska@gmail.com>
2020-04-12 17:34:28 +0200

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -13,14 +13,26 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }*/
+
+
 const titleClickHandler = function(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+ activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.post');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
